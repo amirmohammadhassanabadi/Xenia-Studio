702168332951bd9a6ecf70a5c4d997c7d6ebe236
amir <hasanabadi.am1380@gmail.com>
2024-10-15 15:29:44 +0330

    feat: server created

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
+const express = require("express");
 const config = require("./config")[process.env.NODE_ENV || "development"];
 const logger = config.logger;
+
+// creating App
+const app = express();
+
+// Server Init
+app.listen(config.port, () => {
+    logger.info(`app is running on port ${config}`)
+})
--- config/index.js
+++ config/index.js
@@ -1,15 +1,18 @@
 const { name, version } = require("../package.json");
 const logger = require("pino");
+require("dotenv").config({path: "../.env"});
 
 module.exports = {
   development: {
     name: `${name} - [development]`,
     version,
+    port: process.env.PORT || 4000,    
     logger: logger(),
   },
   production: {
     name,
     version,
+    port: process.env.PORT || 4000,
     logger: logger(),
   },
 };
